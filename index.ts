630e35275c02c88c0f85c1bcf4cdaf94afb99163
mustafa5530180 <atoz5530180@gmail.com>
2024-07-23 02:25:35 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random()*6+1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type:"number",
+        message:chalk.blue(`Guess a Number between (1-6) `),
+    }
+])
+if (answer.userGuessedNumber===randomNumber){
+    console.log(chalk.green("Congtratulation! You Win "))
+} else{
+    console.log(chalk.red("You Guessed Wrong Number. You Loss"))
+}
